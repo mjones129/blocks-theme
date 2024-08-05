2b5f9c552e38cfb98369b88df7e7df72bba9c1be
Matt Jones <me@mattjones.tech>
2024-08-05 14:23:52 -0400

    initial theme file commit

---
+++ patterns/front-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: front-page
+ * Slug: blocks-theme/front-page
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"url":"https://picsum.photos/1920/1080/","dimRatio":50,"align":"center","style":{"color":[]}} -->
+<div class="wp-block-cover aligncenter"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="<?php esc_html_e('', 'blocks-theme');?>" src="https://picsum.photos/1920/1080/" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center"><?php esc_html_e('<strong>This is the homepage.</strong>', 'blocks-theme');?></h1>
+<!-- /wp:heading --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('', 'blocks-theme');?></p>
+<!-- /wp:paragraph -->
